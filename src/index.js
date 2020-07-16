9246b935c18381199d56a923acc87bf085e06fcb
github username <github email address>
2020-07-16 11:07:42 -0500

    halfway done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 let addToy = false;
 
+const TOYS_URL = "http://localhost:3000/toys"
+
+function fetchToys(){
+  fetch(TOYS_URL)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(jsonToys => createToyCard(jsonToys))
+}
+
+function createToyCard(jsonToys){
+  const toyCollectionDiv = document.getElementById("toy-collection");
+  jsonToys.forEach(toy => {
+    const cardDiv = document.createElement("div")
+    cardDiv.className = "card"
+    const toyName = document.createElement("h2")
+    toyName.innerText = toy.name
+    cardDiv.appendChild(toyName)
+    const toyImage = document.createElement("img")
+    toyImage.src = toy.image
+    cardDiv.appendChild(toyImage)
+    const toyLikes = document.createElement("p")
+    toyLikes.innerText = `${toy.likes} Likes`
+    cardDiv.appendChild(toyLikes)
+    const likeButton = document.createElement("button")
+    likeButton.className = "like-btn"
+    cardDiv.appendChild(likeButton)
+
+    toyCollectionDiv.appendChild(cardDiv)
+  })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +44,8 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetchToys();
 });
+
+
